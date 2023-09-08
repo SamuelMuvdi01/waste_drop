3f716abe11ff260aa2ddccff47ccd66dd5cda2c8
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-08 14:23:05 -0400

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Look under this line
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hey guys. GET TO WORK
         </a>
       </header>
     </div>
