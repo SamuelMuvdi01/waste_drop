ea868e7f1a5ae55781a34aecd7140ac0f703d418
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 19:45:20 -0400

    added dq connection back

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,12 @@ import streamlit as st
 import pandas
 import requests
 from urllib.error import URLError
+import mysql.connector
 
 st.title('WasteDrop')
 
 st.write("Home Page")
+
+conn = st.experimental_connection('mysql', type='sql')
+df = conn.query('SELECT * from wastedrop_db.users;', ttl=600)
 
