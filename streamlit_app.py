7c8be76fa0324b4e6d1717f7ec4e1f9877edbcc1
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 19:57:30 -0400

    changed sql connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,14 @@ st.title('WasteDrop')
 
 st.write("Home Page")
 
-conn = st.experimental_connection('mysql', type='sql')
-df = conn.query('SELECT * from wastedrop_db.users;', ttl=600)
+conn = mysql.connector.connect(**st.secrets["mysql"])
 
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+    
+rows = run_query("SELECT * FROM wastedrop_db.users")
+
+print(rows)
