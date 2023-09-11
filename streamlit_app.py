71569c97c09a4c2e3d59a6b5ef603817c9946f26
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 19:34:38 -0400

    removed db conection

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,3 @@ st.title('WasteDrop')
 
 st.write("Home Page")
 
-conn = st.experimental_connection('mysql', type='sql')
-
-# Perform query.
-df = conn.query('SELECT * from wastedrop_db.users;', ttl=600)
-
-print(df)
