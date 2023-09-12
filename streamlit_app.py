a4753d9794d8ee1bf9d49279ae6107c1f2eef805
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 01:04:43 -0400

    added print text

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,28 +23,4 @@ def run_query(query):
     
 rows = run_query("SELECT * FROM public.users")
 
-
-
-
-
-#cnx = mysql.connector.connect(user="wastedrop_admin", password="Team_7_pass$", host="waste-drop-server.mysql.database.azure.com", port=3306, database="wastedrop_db", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
-#cursor = cnx.cursor()
-#cursor.execute("SELECT * from wastedrop_db.users")
-#result = cursor.fetchall()
-#print(result)
-
-
-
-"""
-conn = mysql.connector.connect(**st.secrets["mysql"])
-
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-    
-rows = run_query("SELECT * FROM wastedrop_db.users")
-
-print(rows)
-"""
+st.write(rows)
