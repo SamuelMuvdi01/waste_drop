5055814d318bbcc1eb72c90101308448ff824f56
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 23:34:08 -0400

    changed to pysql

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,29 @@ import streamlit as st
 import pandas
 import requests
 from urllib.error import URLError
-
+import pymysql
 
 
 st.title('WasteDrop')
 
 st.write("Home Page")
+
+conn = pymysql.connect(
+    host=st.secrets.mysql.host,
+    user=st.secrets.mysql.user,
+    password=st.secrets.mysql.password,
+    port=st.secrets.mysql.port,
+    database=st.secrets.mysql.database,
+    ssl_disabled=st.secrets.mysql.ssl_disabed
+    )
+
+curr = conn.cursor()
+curr.execute('SELECT * FROM wastedrop_db.users')
+output = curr.fetchall()
+
+for i in output:
+    print(i)
+
 #cnx = mysql.connector.connect(user="wastedrop_admin", password="Team_7_pass$", host="waste-drop-server.mysql.database.azure.com", port=3306, database="wastedrop_db", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
 #cursor = cnx.cursor()
 #cursor.execute("SELECT * from wastedrop_db.users")
@@ -15,12 +32,7 @@ st.write("Home Page")
 #print(result)
 
 
-conn = st.experimental_connection('mysql', type='sql')
 
-df = conn.query('SELECT * FROM wastedrop_db.users')
-
-for row in df.itertuples():
-    st.write(f"{row.first_name}")
 """
 conn = mysql.connector.connect(**st.secrets["mysql"])
 
