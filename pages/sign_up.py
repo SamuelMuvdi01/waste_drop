51af2197d568c8a73b7367be162a134631d0ab1f
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 19:29:53 -0400

    added clear button

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -7,25 +7,15 @@ import re
 st.set_page_config(page_title="sign_up")
 st.title("welcome, please sign up below")
 
-def clear_inputs():
-    st.session_state.first_name = "",
-    st.session_state.last_name = "",
-    st.session_state.email = "",
-    st.session_state.password = ""
-
 
 first_name = st.text_input("Please enter first name", placeholder="John", key="first_name")
 last_name = st.text_input("Please enter last name", placeholder="Doe", key="last_name")
 email = st.text_input("Please enter email", placeholder="JohnDoe@gmail.com", key="email")
 password = st.text_input("Please enter a password", key="password", placeholder="xxxxxxxx", help="Password must be at least 8 characters long, have an upper case letter, and have a symbol")
 create_user_button = st.button("Create account", key="create_user_button")
-clear_button = st.button("Clear", on_click=clear_inputs)
+clear_button = st.button("Clear", on_click=st.empty())
 pass_valid= bool(re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$", password ))
 
-if(pass_valid == True):
-    st.session_state.disabled = False
-elif (pass_valid == False):
-    st.session_state.disabled = True
 
 
 
