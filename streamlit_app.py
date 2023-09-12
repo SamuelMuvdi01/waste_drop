96fd301b7af03cca13c7ca79150354f10c11e7c1
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 16:04:12 -0400

    moved page config

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
 import streamlit as st
+
+st.set_page_config(
+    page_title='home_page'
+)
+
 import pandas as pd
 from urllib.error import URLError
 import psycopg2
 
-st.set_page_config(
-    page_title="home_page"
-)
+
 st.sidebar.success("Selecta page above")
 
 st.title('WasteDrop')
