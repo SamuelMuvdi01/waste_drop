5b819697d964447fa1c16e712180fe2db39b5fba
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 19:51:23 -0400

    re added clear button

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -8,18 +8,37 @@ st.set_page_config(page_title="sign_up")
 st.title("welcome, please sign up below")
 
 
+
+
+
 first_name = st.text_input("Please enter first name", placeholder="John", key="first_name")
 last_name = st.text_input("Please enter last name", placeholder="Doe", key="last_name")
 email = st.text_input("Please enter email", placeholder="JohnDoe@gmail.com", key="email")
 password = st.text_input("Please enter a password", key="password", placeholder="xxxxxxxx", help="Password must be at least 8 characters long, have an upper case letter, and have a symbol")
 create_user_button = st.button("Create account", key="create_user_button")
-clear_button = st.button("Clear")
+
+
+if first_name not in st.session_state:
+    st.session_state['first_name']
+if last_name not in st.session_state:
+    st.session_state['last_name']
+if email not in st.session_state:
+    st.session_state['email']
+if password not in st.session_state:
+    st.session_state['password']
+
+
+def clear_inputs():
+    st.session_state['first_name'] = ""
+    st.session_state['last_name'] = ""
+    st.session_state['email'] = ""
+    st.session_state['password'] = ""
+
+
+
+clear_button = st.button("Clear", on_click=clear_inputs)
 pass_valid= bool(re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$", password ))
 
 
-if(clear_button):
-    first_name = st.empty(),
-    last_name = st.empty(),
-    email = st.empty(),
-    password = st.empty()
+
 
