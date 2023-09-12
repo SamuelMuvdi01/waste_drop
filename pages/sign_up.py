06500c20f363aa386c9ec26a2ad3ddf668091220
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 12:49:00 -0400

    added sign up config

---
+++ pages/sign_up.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit.source_util import _on_pages_changed, get_pages
+from streamlit_extras.switch_page_button import switch_page
+
+st.set_page_config(page_title="sign_up")
+st.write("welcome, please sign up below")
