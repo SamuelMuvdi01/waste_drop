6b4f18f9e8957dcf11654f24f6963a179296aa25
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 23:35:37 -0400

    erased reqs

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,18 +31,3 @@ for i in output:
 #result = cursor.fetchall()
 #print(result)
 
-
-
-"""
-conn = mysql.connector.connect(**st.secrets["mysql"])
-
-@st.cache_data(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-    
-rows = run_query("SELECT * FROM wastedrop_db.users")
-
-print(rows)
-"""
