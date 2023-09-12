068e9da91bbbbd5c62acbaddf9f890c48b60a17f
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-12 11:52:28 -0400

    added sign up page link

--- pages/login.py
+++ pages/login.py
@@ -1,5 +1,12 @@
 import streamlit as st
+from pages import login
+from multiapp import MultiApp
+from pages import sign_up
+app = MultiApp
 
 st.set_page_config(page_title="login")
 
 st.title("Please login!")
+
+
+app.add_app("Sign Up", sign_up.app)
