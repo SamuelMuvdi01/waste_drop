f4253b28f4b245d7e01099601b11e0e325a79205
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-11 20:28:23 -0400

    changed again connector type

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,18 @@ import requests
 from urllib.error import URLError
 import mysql.connector
 
+
 st.title('WasteDrop')
 
 st.write("Home Page")
 
+cnx = mysql.connector.connect(user="wastedrop_admin", password="Team_7_pass$", host="waste-drop-server.mysql.database.azure.com", port=3306, database="wastedrop_db", ssl_ca="DigiCertGlobalRootCA.crt.pem", ssl_disabled=False)
+
+cursor = cnx.cursor()
+
+query = ("SELECT * from wastedrop_db.users")
+
+"""
 conn = mysql.connector.connect(**st.secrets["mysql"])
 
 @st.cache_data(ttl=600)
@@ -19,3 +27,4 @@ def run_query(query):
 rows = run_query("SELECT * FROM wastedrop_db.users")
 
 print(rows)
+"""
