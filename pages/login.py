eeb17c20039748b1e750fff37759ced648401769
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-15 14:49:10 -0400

    added some login fields

--- pages/login.py
+++ pages/login.py
@@ -1,12 +1,25 @@
 import streamlit as st
 from streamlit.source_util import _on_pages_changed, get_pages
 from streamlit_extras.switch_page_button import switch_page
-
+import psycopg2
 
 st.set_page_config(page_title="Login")
 
 st.title("Please login!")
 
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+    
+email_login = st.text_input("Please enter email", placeholder="JohnDoe@gmail.com")
+password_login = st.text_input("Please enter password", placeholder="*****")
 
 if st.button("Sign Up"):
     switch_page("sign_up")
