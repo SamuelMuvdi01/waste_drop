f3a77a89e3b7081978e15c069514018ecab75d57
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-15 15:18:55 -0400

    used zip dict

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -51,4 +51,5 @@ pass_valid= bool(re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}
 
 if(pass_valid == True and len(last_name_val) > 2 and len(last_name_val) > 2 and pass_email == True):
     hashed_password = stauth.Hasher(password_val).generate()
-    run_query("INSERT INTO public.users(email, first_name, last_name, password) VALUES({}, {}, {}, {})".format(email_val, first_name_val, last_name_val, hashed_password))
+    for (email_vals, first_name_vals, last_name_vals, hashed_passwords) in zip(email_val, first_name_val, last_name_val, hashed_password):
+        run_query("INSERT INTO public.users(email, first_name, last_name, password) VALUES({}, {}, {}, {})".format(email_vals, first_name_vals, last_name_vals, hashed_passwords))
