03da3de04eea552c888818103402676f3ba12012
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-15 15:49:46 -0400

    fixed syntax

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -51,5 +51,5 @@ if(pass_valid == True and len(last_name_val) > 2 and len(last_name_val) > 2 and 
     #hashed_password = stauth.Hasher(password_val).generate()
    #for (hashed_passwords) in (hashed_password):
         run_query("INSERT INTO public.users(email, first_name, last_name, password) VALUES({}, {}, {}, {})".format(email_val, first_name_val, last_name_val, password_val))
-conn.comit()
+conn.commit()
 st.write("Account created!")
