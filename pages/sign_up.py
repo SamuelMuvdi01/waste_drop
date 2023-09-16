b65f38ef3d28fb5b02e172d5d7b2f915ee6f1439
Edward Basulto <48189137+Ebasu004@users.noreply.github.com>
2023-09-15 20:16:32 -0400

    Fixed

--- pages/sign_up.py
+++ pages/sign_up.py
@@ -42,30 +42,24 @@ create_user_button = st.button("Create account", key="create_user_button")
 
 clear_button = st.button("Clear", on_click=clear_inputs)
 pass_email = bool(re.match(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b', email_val))
-pass_valid = bool(re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=]).{8,}$", password_val ))
+pass_valid = bool(re.match(r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$", password_val))
 
 
 if create_user_button:
-    if pass_valid:
-        st.write("pass valid")
-    if pass_email:
-        st.write("email valid")
-    if len(last_name_val) > 2:
-        st.write("last name valid")
-    if len(first_name_val) > 2:
-        st.write("first name valid")
+    if pass_valid and len(last_name_val) > 2 and len(first_name_val) > 2 and pass_email:
+        hashed_password = stauth.Hasher(password_val).generate()
+        hashed_password = str(hashed_password[1])
 
-    # if pass_valid and len(last_name_val) > 2 and len(first_name_val) > 2 and pass_email:
-    #     hashed_password = stauth.Hasher(password_val).generate()
-    #     hashed_password = str(hashed_password[1])
-
-    #     try:
-    #         cursor.execute("INSERT INTO public.users(email, first_name, last_name, password) VALUES('{}', '{}', '{}', '{}')".format(email_val, first_name_val, last_name_val, hashed_password))
-    #         conn.commit()
-    #         st.write("Account created!")
-    #     except Exception as e:
-    #         st.error(f"Error creating account: {e}")
+        try:
+            cursor.execute("INSERT INTO public.users(email, first_name, last_name, password) VALUES('{}', '{}', '{}', '{}')".format(email_val, first_name_val, last_name_val, hashed_password))
+            conn.commit()
+            st.write("Account created!")
+        except Exception as e:
+            st.error(f"Error creating account: {e}")
             
-    #     clear_inputs()
-    #     switch_page("login")
+        clear_inputs()
+        switch_page("login")
+
+    else:
+        st.write("One of the fields above are invalid.")
 
