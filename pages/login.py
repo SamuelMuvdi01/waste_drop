09ef17e7abe9081f74980c7a5f04d73bcb370dfb
Edward Basulto <48189137+Ebasu004@users.noreply.github.com>
2023-09-15 21:01:39 -0400

    login.py update

--- pages/login.py
+++ pages/login.py
@@ -5,40 +5,41 @@ from streamlit_extras.switch_page_button import switch_page
 import psycopg2
 
 st.set_page_config(page_title="Login")
-
-st.title("Please login!")
+st.title("Welcome, please log in below")
 
 @st.cache_resource
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
+conn.autocommit = True
+cursor = conn.cursor()
 
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-    
-def verify_login(email, password):
-    hashed_password = stauth.Hasher(password).generate()
-    hashed_password = str(hashed_password[1])
-    query = f"SELECT * FROM public.users WHERE email = '{email}' AND password = '{hashed_password}'"
-    result = run_query(query)
-    return len(result) > 0
+def authenticate_user(email, password):
+    cursor.execute("SELECT * FROM public.users WHERE email=%s", (email,))
+    user = cursor.fetchone()
+    if user:
+        hashed_password = user[3]
+        # Add a '[' character to the front of the stored hashed password
+        hashed_password = '[' + hashed_password
+        if stauth.Hasher.verify(password, hashed_password):
+            return True
+    return False
 
-email_login = st.text_input("Please enter email", placeholder="JohnDoe@gmail.com")
-password_login = st.text_input("Please enter password", type="password", placeholder="********")
+email_val = st.text_input("Email", key="email")
+password_val = st.text_input("Password", key="password", type="password")
 
-login_button = st.button("Login")
+login_button = st.button("Login", key="login_button")
 
 if login_button:
-    hashed_password = stauth.Hasher(password_login).generate()
-    hashed_password = str(hashed_password[1])
-    if verify_login(email_login, hashed_password):
-        st.write("Login successful!")
-        switch_page("home")
+    if email_val and password_val:
+        if authenticate_user(email_val, password_val):
+            st.write("Login successful!")
+            # Add code here to redirect to a dashboard or another page
+        else:
+            st.error("Invalid email or password")
     else:
-        st.write("Invalid email or password.")
+        st.error("Please enter both email and password")
 
 if st.button("Sign Up"):
     switch_page("sign_up")
