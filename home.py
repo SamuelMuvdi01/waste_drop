fdcc069d998fcb3c2d4535e8d342f68c9a0fa407
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-16 23:50:50 -0400

    moved location of name config

--- home.py
+++ home.py
@@ -1,3 +1,4 @@
+st.set_page_config(page_title="Home")
 import streamlit as st
 import pandas as pd
 from urllib.error import URLError
@@ -5,8 +6,6 @@ from streamlit.source_util import _on_pages_changed, get_pages
 from streamlit_extras.switch_page_button import switch_page
 import sys
 from pages import login
-
-st.set_page_config(page_title="Home")
 
 st.sidebar.success("Sign up or login!")
 
