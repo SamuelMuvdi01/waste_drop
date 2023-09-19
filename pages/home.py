3038525259e36099eafcbf6cc849291d8b912abf
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-18 22:58:19 -0400

    added new if

--- pages/home.py
+++ pages/home.py
@@ -6,10 +6,12 @@ from streamlit.source_util import _on_pages_changed, get_pages
 from streamlit_extras.switch_page_button import switch_page
 import sys
 from extra_streamlit_components import CookieManager
-import pages.login
 
 if "login_status" not in st.session_state:
     st.session_state['login_status'] = False
+
+if "saved_user_name" not in st.session_state:
+    st.session_state["saved_user_name"] = ""
 
 
 def log_out():
@@ -21,7 +23,7 @@ st.title('WasteDrop')
 
 
 if(st.session_state["login_status"] == True):
-    st.write("Welcome! ",pages.login.st.session_state["saved_user_name"])
+    st.write("Welcome! ",st.session_state["saved_user_name"])
     logout_button = st.sidebar.button("log off", on_click=log_out)
     
 
