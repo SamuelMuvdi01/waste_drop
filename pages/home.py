30e4b45fc048561d37a5a95d5daa68f4d2e03cbd
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-18 22:40:54 -0400

    removed hide pages

--- pages/home.py
+++ pages/home.py
@@ -5,25 +5,17 @@ from urllib.error import URLError
 from streamlit.source_util import _on_pages_changed, get_pages
 from streamlit_extras.switch_page_button import switch_page
 import sys
-from st_pages import Page, show_pages, hide_pages
 from extra_streamlit_components import CookieManager
 
 if "login_status" not in st.session_state:
     st.session_state['login_status'] = False
 
-show_pages([
-    Page("sign_up.py","sign_up"),
-    Page("pages/login.py", "login")
-        ])
 
 
 def log_out():
     st.session_state['login_status'] = False
     switch_page("sign_up")
-    hide_pages([
-    Page("sign_up.py","sign_up"),
-    Page("pages/login.py", "login")
-        ])
+
     
 
 st.title('WasteDrop')
