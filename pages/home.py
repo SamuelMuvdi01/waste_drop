6e0c724ec28173d89d940ba74fd4028287fe163d
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-09-18 23:00:51 -0400

    changed name type

--- pages/home.py
+++ pages/home.py
@@ -23,7 +23,7 @@ st.title('WasteDrop')
 
 
 if(st.session_state["login_status"] == True):
-    st.write("Welcome! ",st.session_state["saved_user_name"])
+    st.write("Welcome! ",str.join(st.session_state["saved_user_name"]))
     logout_button = st.sidebar.button("log off", on_click=log_out)
     
 
