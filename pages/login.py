e481c2524363b67d66a492dd991663ff93d479ae
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-10-05 21:28:46 -0400

    str

--- pages/login.py
+++ pages/login.py
@@ -55,7 +55,7 @@ if login_button:
 
     @st.cache_data
     def save_user_id():
-        st.write("id val as string", query_df["id"].values)
+        st.write("id val as string", str(query_df["id"].values))
         st.session_state["saved_user_id"] = query_df["id"].values
 
     if(hashed_password == query_df["password"].values):
