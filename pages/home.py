69c8c6c20c979a5e6b891819517a2be70f08a001
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-10-05 20:55:14 -0400

    added sesh state

--- pages/home.py
+++ pages/home.py
@@ -10,15 +10,14 @@ import psycopg2
 import helperfuncs as hf
 from pages.login import conn
 
-
-
 if "saved_user_name" not in st.session_state:
     st.session_state["saved_user_name"] = ""
 
+if "saved_user_id" not in st.session_state:
+    st.session_state["saved_user_id"] = ""
 
 if "login_status" not in st.session_state:
     st.session_state['login_status'] = False
-
 
 def log_out():
     st.session_state['login_status'] = False
