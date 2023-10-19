34519dc1649aeeb37e3acbfd9efe053276013897
edward <edward.basulto29@gmail.com>
2023-10-18 22:16:43 -0400

    binz page removal

--- pages/binz.py
+++ pages/binz.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit.source_util import _on_pages_changed, get_pages
 import streamlit_authenticator as stauth
 from streamlit_extras.switch_page_button import switch_page
 import psycopg2
