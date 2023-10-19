3f907576459e881a6a6179e372d1b3cf0c3b58ca
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-10-19 18:30:06 -0400

    added logout session state

--- pages/my_bin.py
+++ pages/my_bin.py
@@ -7,27 +7,34 @@ import sys
 from extra_streamlit_components import CookieManager
 import psycopg2
 import helperfuncs as hf
-from pages.login import conn
+from pages.home import conn
 
-st.title(st.session_state["selected_binz"])
-
-if "saved_user_id" not in st.session_state:
-    st.session_state["saved_user_id"] = ""
-
-
-cursor = conn.cursor()
-
-def log_out():
+if "login_status" not in st.session_state:
     st.session_state['login_status'] = False
-    switch_page("sign_up")
 
-binz_item = st.text_input("Add an Item to bin: ")
-exp_date = st.date_input("Please Enter Item Expiration: ")
-count = st.number_input("Please Enter Item Quantity: ", step=1, min_value=1)
-add_binz_item = st.button("Add Item")
-binz_uuid = st.session_state["selected_binz"]
-logout_button = st.sidebar.button("Log Off", on_click=log_out)
+if(st.session_state["login_status"] == False):
+    st.write('PLEASE LOG IN!')
+else:
 
-cursor.execute("INSERT INTO public.items(binz_id, quantity, expiry_date) VALUES('{}', '{}', '{}')".format(binz_uuid, count, exp_date))
-conn.commit()
-st.write(":green[Item Added!]")
+
+    st.title(st.session_state["selected_binz"])
+
+    if "saved_user_id" not in st.session_state:
+        st.session_state["saved_user_id"] = ""
+    cursor = conn.cursor()
+
+    def log_out():
+        st.session_state['login_status'] = False
+        switch_page("sign_up")
+
+    
+    binz_item = st.text_input("Add an Item to bin: ")
+    exp_date = st.date_input("Please Enter Item Expiration: ")
+    count = st.number_input("Please Enter Item Quantity: ", step=1, min_value=1)
+    add_binz_item = st.button("Add Item")
+    binz_uuid = st.session_state["selected_binz"]
+    logout_button = st.sidebar.button("Log Off", on_click=log_out)
+
+    cursor.execute("INSERT INTO public.items(binz_id, quantity, expiry_date) VALUES('{}', '{}', '{}')".format(binz_uuid, count, exp_date))
+    conn.commit()
+    st.write(":green[Item Added!]")
