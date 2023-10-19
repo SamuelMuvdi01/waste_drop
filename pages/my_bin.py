170f0794e4786fe0db51341f79af4292838e895a
SamuelMuvdi01 <78106875+SamuelMuvdi01@users.noreply.github.com>
2023-10-19 18:16:19 -0400

    added min val 1

--- pages/my_bin.py
+++ pages/my_bin.py
@@ -17,7 +17,7 @@ def log_out():
 
 binz_item = st.text_input("Add an Item to bin: ")
 exp_date = st.date_input("Please Enter Item Expiration: ")
-count = st.number_input("Please Enter Item Quantity: ", step=1)
+count = st.number_input("Please Enter Item Quantity: ", step=1, min_value=1)
 add_binz_item = st.button("Add Item")
 
 logout_button = st.sidebar.button("Log Off", on_click=log_out)
