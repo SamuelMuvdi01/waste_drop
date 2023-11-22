e3d37fed9e84c99c7232a330338aa26b74d3dd24
edward <edward.basulto29@gmail.com>
2023-11-22 12:50:57 -0500

    upload photo

--- pages/home.py
+++ pages/home.py
@@ -33,6 +33,8 @@ def rename_bin(old_bin_name, new_bin_name, user_id):
     conn.commit()
     st.success(f":green[Binz '{old_bin_name}' renamed to '{new_bin_name}']")
 
+image_path = "WD.jpg"
+
 st.title('WasteDrop')
 
 if st.session_state["login_status"] == True:
